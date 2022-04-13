865a12005e0c21f7778cc392ae1c2bafc6406617
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2022-04-13 07:40:33 -0700

    Uncomment recommendation fetching

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -3,6 +3,7 @@
   import Tailwindcss from "./Tailwindcss.svelte";
   import Landing from "./Landing.svelte";
   import SearchList from "./SearchList.svelte";
+  import to from "await-to-js";
 
   let isSearching = false;
   let isLoading = false;
@@ -13,28 +14,28 @@
     isSearching = true;
     isLoading = true;
     console.log(query);
-    // const [err, res] = await to(fetch("https://recommeddit.nrp-nautilus.io/search?"
-    //   + new URLSearchParams({ query })));
-    // if (err || !res.ok) {
-    //   error = true;
-    //   return;
-    // }
-    // const parsedRes = await res.json();
-    // recommendations = (parsedRes?.recommendations ?? []).map(({ recommendation, images, score, link, comments }) => {
-    //   return {
-    //     keyword: recommendation,
-    //     description: "",
-    //     score,
-    //     imageUrls: images,
-    //     comments: comments.map(comment => ({
-    //       text: comment["body text"],
-    //       score: comment.score,
-    //       authorName: comment["author name"],
-    //       url: comment.permalink
-    //     })),
-    //     link
-    //   };
-    // });
+    const [err, res] = await to(fetch("https://recommeddit.nrp-nautilus.io/search?"
+      + new URLSearchParams({ query })));
+    if (err || !res.ok) {
+      error = true;
+      return;
+    }
+    const parsedRes = await res.json();
+    recommendations = (parsedRes?.recommendations ?? []).map(({ recommendation, images, score, link, comments }) => {
+      return {
+        keyword: recommendation,
+        description: "",
+        score,
+        imageUrls: images,
+        comments: comments.map(comment => ({
+          text: comment["body text"],
+          score: comment.score,
+          authorName: comment["author name"],
+          url: comment.permalink
+        })),
+        link
+      };
+    });
     error = true;
     isLoading = false;
   };
